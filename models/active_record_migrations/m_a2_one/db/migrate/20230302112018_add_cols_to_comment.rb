29180d669d593049247e4f58b1480fda0e927843
itsAndL <ayoub003lkebch@gmail.com>
2023-03-02 13:06:17 +0100

    trying the add_foreign_key method

--- models/active_record_migrations/m_a2_one/db/migrate/20230302112018_add_cols_to_comment.rb
+++ models/active_record_migrations/m_a2_one/db/migrate/20230302112018_add_cols_to_comment.rb
@@ -2,6 +2,6 @@ class AddColsToComment < ActiveRecord::Migration[7.0]
   def change
     add_column :comments, :resource_id, :integer, foreign_key: true
     add_index :comments, :resource_id
-    add_reference :comments, :author, foreign_key: true
+    add_reference :comments, :author
   end
 end
--- models/active_record_migrations/m_a2_one/db/schema.rb
+++ models/active_record_migrations/m_a2_one/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_112018) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_115511) do
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
