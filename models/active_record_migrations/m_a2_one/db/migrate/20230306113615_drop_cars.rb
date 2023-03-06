a269c9a72859feb11322d3b05fdadea7ccb5d901
itsAndL <ayoub003lkebch@gmail.com>
2023-03-06 12:46:47 +0100

    make drop_table reversible

--- models/active_record_migrations/m_a2_one/db/migrate/20230306113615_drop_cars.rb
+++ models/active_record_migrations/m_a2_one/db/migrate/20230306113615_drop_cars.rb
@@ -1,5 +1,16 @@
 class DropCars < ActiveRecord::Migration[7.0]
-  def change
+  def up
     drop_table :cars
+  end
+
+  def down
+    create_table "cars", primary_key: "car_id", force: :cascade do |t|
+      t.string "model"
+      t.string "comapany"
+      t.integer "book_id"
+      t.date "year"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
   end
 end
