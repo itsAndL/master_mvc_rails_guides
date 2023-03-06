5a02a7e24b9ee97ea8a226213c52a102530c3d50
itsAndL <ayoub003lkebch@gmail.com>
2023-03-06 21:47:24 +0100

    inline attributes for validation

--- models/active_record_validation/m_a3_one/app/models/person.rb
+++ models/active_record_validation/m_a3_one/app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  validates :first_name, :last_name, presence: true
 end
