608f429d95e06e1557fe7e824c60f6e4e46d9646
itsAndL <ayoub003lkebch@gmail.com>
2023-03-10 12:39:12 +0100

    customise the default message for validation methods

--- models/active_record_validation/m_a3_one/app/models/person.rb
+++ models/active_record_validation/m_a3_one/app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ApplicationRecord
   # validates :first_name, presence: true
   # validates :last_name, presence: true
-  validates :first_name, :last_name, presence: true
-  validates :adult, acceptance: true
+  validates :first_name, :last_name, presence: { message: 'must be given' }
+  validates :adult, acceptance: { message: 'must be checked' }
 end
