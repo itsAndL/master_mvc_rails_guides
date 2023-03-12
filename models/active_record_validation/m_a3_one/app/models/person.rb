95a8d9a64fbb0f07ee70a098140e6c7ab2e3b51e
itsAndL <ayoub003lkebch@gmail.com>
2023-03-12 16:42:18 +0100

    using the comparison helper

--- models/active_record_validation/m_a3_one/app/models/person.rb
+++ models/active_record_validation/m_a3_one/app/models/person.rb
@@ -1,11 +1,15 @@
 class Person < ApplicationRecord
   # validates :first_name, presence: true
   # validates :last_name, presence: true
-  validates :first_name, :last_name, presence: { message: 'must be given' }
+  validates :first_name, :last_name, :age, presence: { message: 'must be given' }
   validates :adult, acceptance: { message: 'must be checked' }
 
   # has_many :homes, dependent: :delete_all
 
   belongs_to :asset, polymorphic: true
   validates_associated :asset, message: "should be available"
+
+  validates :age, comparison: { greater_than_or_equal_to: 18 , less_than: 40}
+  # validates :first_name, comparison: { equal_to: proc { "James" } }
+  validates :first_name, comparison: { equal_to: "James" }
 end
