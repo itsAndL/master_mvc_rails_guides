a1db3a54d0bc96de7db8b5e5ee9351c330b655e5
itsAndL <ayoub003lkebch@gmail.com>
2023-03-12 12:19:51 +0100

    palying with configuration options of validates_associated method

--- models/active_record_validation/m_a3_one/app/models/home.rb
+++ models/active_record_validation/m_a3_one/app/models/home.rb
@@ -2,5 +2,5 @@ class Home < ApplicationRecord
     belongs_to :person
     validates :home_type, presence: { message: 'must be given' }
     
-    validates_associated :person
+    validates_associated :person, message: "need to be validate", if: lambda { 2==2 }
 end
