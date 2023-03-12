7daeec1816cc62d0972bd2e07a9109a07615f90a
itsAndL <ayoub003lkebch@gmail.com>
2023-03-12 17:10:32 +0100

    using the exclusion helper

--- models/active_record_validation/m_a3_one/app/models/car.rb
+++ models/active_record_validation/m_a3_one/app/models/car.rb
@@ -2,4 +2,5 @@ class Car < ApplicationRecord
     has_one :person, as: :asset
 
     validates :company, presence: { message: 'must be provided for us' }
+    validates :company, exclusion: { case_sensitive: false, within: %w(acura alfa_romeo audi), message: '%{value} is reserved' }
 end
