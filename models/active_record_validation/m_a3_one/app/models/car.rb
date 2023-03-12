87bb009ae3c88ae1922003c2ba7343abf16514f2
itsAndL <ayoub003lkebch@gmail.com>
2023-03-12 17:57:23 +0100

    using the format helper

--- models/active_record_validation/m_a3_one/app/models/car.rb
+++ models/active_record_validation/m_a3_one/app/models/car.rb
@@ -1,6 +1,7 @@
 class Car < ApplicationRecord
     has_one :person, as: :asset
 
-    validates :company, presence: { message: 'must be provided for us' }
+    validates :company, :model, presence: { message: 'must be provided for us' }
     validates :company, exclusion: { case_sensitive: false, within: %w(acura alfa_romeo audi), message: '%{value} is reserved' }
+    validates :model, format: {with: /[A-Z][0-9][0-9][0-9]/, message: "should valid this format A000"}
 end
